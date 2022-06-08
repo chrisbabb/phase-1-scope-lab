74d0a2cf64627364c1ba88e16422a988d1349f4e
Christopher Babb <chris@appsoftdev.com>
2022-06-08 09:28:16 -0700

    commit tree 6bc28c4533a7a656507cf1c95917e2c2cd412260

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "jeff"
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(overwrite) {
+    bestCustomer = overwrite
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not jeff"
+}
